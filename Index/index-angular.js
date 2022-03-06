13786bbd03bbc22fb3728a03b767afdaeed6b397
nickhughes34 <101068746+nickhughes34@users.noreply.github.com>
2022-03-06 17:16:58 -0500

    Update index-angular.js
    Added Code variable

--- Index/index-angular.js
+++ Index/index-angular.js
@@ -4,7 +4,8 @@ app.controller("angular", function($scope){
   var pythonPositions = ["first", "second", "third", "fourth"];
   var frontPositions = ["first", "second", "third", "fourth"];
   var backPositions = ["first", "second"];
-
+  var code = "https://github.com/nickhughes34/nickhughes34.github.io";
+  
   /*$scope.python = [
     {"Name": "Unkown Webpage 1", "Image": "Images/Screenshots/survey-screenshot.jpg", "Alt": "unknown page screenshot", "Go": "#", "Code": "#", "Position": "first"},
   {"Name": "Unkown Webpage 2", "Image": "Images/Screenshots/survey-screenshot.jpg", "Alt": "unknown page screenshot", "Go": "#", "Code": "#", "Position": "second"},
@@ -12,15 +13,15 @@ app.controller("angular", function($scope){
   {"Name": "Unkonwn Webpage 4", "Image": "Images/Screenshots/survey-screenshot.jpg", "Alt": "unknown page screenshot", "Go": "#", "Code": "#", "Position": "fouth"}];*/
 
   $scope.frontend = [
-    {"Name": "Product Webpage", "Image": "Images/Screenshots/product-screenshot-2.jpg", "Alt": "product page screenshot", "Go": "/Product/product.html", "Code": "#", "Position": "first"},
-    {"Name": "Survey Webpage", "Image": "Images/Screenshots/survey-screenshot-2.jpg", "Alt": "survey page screenshot", "Go": "/Survey/survey.html", "Code": "#", "Position": "second"},
+    {"Name": "Product Webpage", "Image": "Images/Screenshots/product-screenshot-2.jpg", "Alt": "product page screenshot", "Go": "/Product/product.html", "Code": code, "Position": "first"},
+    {"Name": "Survey Webpage", "Image": "Images/Screenshots/survey-screenshot-2.jpg", "Alt": "survey page screenshot", "Go": "/Survey/survey.html", "Code": code, "Position": "second"},
     //{"Name": "Apple Webpage Clone", "Image": "Images/Screenshots/survey-screenshot.jpg", "Alt": "apple clone page screenshot", "Go": "/Apple_clone/apple_clone.html", "Code": "#", "Position": "third"},
-    {"Name": "Google Clone", "Image": "Images/Screenshots/google-clone-screenshot.jpg", "Alt": "google clone screenshot", "Go": "/Google_Clone/google_clone.html", "Code": "#", "Position": "third"},
-    {"Name": "Animations", "Image": "Images/Screenshots/animations-screenshot.jpg", "Alt": "animations page screenshot", "Go": "/Animation/animation.html", "Code": "#", "Position": "fourth"}];
+    {"Name": "Google Clone", "Image": "Images/Screenshots/google-clone-screenshot.jpg", "Alt": "google clone screenshot", "Go": "/Google_Clone/google_clone.html", "Code": code, "Position": "third"},
+    {"Name": "Animations", "Image": "Images/Screenshots/animations-screenshot.jpg", "Alt": "animations page screenshot", "Go": "/Animation/animation.html", "Code": code, "Position": "fourth"}];
 
   $scope.backend = [
-    {"Name": "Weather Webpage", "Image": "Images/Screenshots/weather-screenshot-2.jpg", "Alt": "weather webpage screenshot", "Go": "/Weather/weather.html", "Code": "#", "Position": "first"},
-  //{"Name": "Weather Webpage", "Image": "Images/Screenshots/weather-screenshot-2.jpg", "Alt": "weather webpage screenshot", "Go": "/Weather/weather.html", "Code": "#", "Position": "second"}
+    {"Name": "Weather Webpage", "Image": "Images/Screenshots/weather-screenshot-2.jpg", "Alt": "weather webpage screenshot", "Go": "/Weather/weather.html", "Code": code, "Position": "first"},
+  //{"Name": "Weather Webpage", "Image": "Images/Screenshots/weather-screenshot-2.jpg", "Alt": "weather webpage screenshot", "Go": "/Weather/weather.html", "Code": code, "Position": "second"}
 ];
 
   $scope.description1 = "I am currently attending Ontario Tech University studying Networking and IT Security. Over the years of attending this school I have gained various skills. Some of them would include network design, python programming, front end and back end web design. I have developed some test webpages such as a Weather Checker, Product Webpage, and more listed below. I have also used various back end technologies such as API's, Amazon's RDS and MongoDB.";
