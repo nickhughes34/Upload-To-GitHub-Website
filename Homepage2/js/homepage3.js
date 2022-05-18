4d85d71e44832af2be5244d9629b35cbb50585bd
nickhughes34 <101068746+nickhughes34@users.noreply.github.com>
2022-05-18 16:38:35 -0400

    Delete Homepage2 directory

--- Homepage2/js/homepage3.js
+++
@@ -1,8 +0,0 @@
-var backgroundVideo =  document.getElementById('backgroundVideo')
-
-backgroundVideo.addEventListener('timeupdate', function() {
-var videoCurrentTime = backgroundVideo.currentTime;
-    if (videoCurrentTime >= 12) {
-        backgroundVideo.pause()
-      }
-});
