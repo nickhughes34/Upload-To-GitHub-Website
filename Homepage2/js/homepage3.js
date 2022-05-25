bd3badad04772813c82a03c4f36da364cd290bb4
nickhughes34 <101068746+nickhughes34@users.noreply.github.com>
2022-05-25 15:22:29 -0400

    Update Homepage2

--- Homepage2/js/homepage3.js
+++ Homepage2/js/homepage3.js
@@ -1,8 +1,34 @@
-var backgroundVideo =  document.getElementById('backgroundVideo')
+var open =  document.getElementById('open');
+var close =  document.getElementById('close');
+var first =  document.getElementById('first');
+var second =  document.getElementById('second');
+var sideNav =  document.getElementById('sideNav');
 
-backgroundVideo.addEventListener('timeupdate', function() {
-var videoCurrentTime = backgroundVideo.currentTime;
-    if (videoCurrentTime >= 12) {
-        backgroundVideo.pause()
-      }
+open.addEventListener('click', function() {
+
+  if (sideNav.classList.contains("block")){
+      sideNav.classList.remove("block");
+  }
+
+  sideNav.classList.toggle("hidden");
+  sideNav.classList.toggle("show");
+  first.classList.toggle("hidden2");
+  first.classList.toggle("show2");
+  second.classList.toggle("hidden2");
+  second.classList.toggle("show2");
+});
+
+close.addEventListener('click', function() {
+  sideNav.classList.toggle("hidden");
+  sideNav.classList.toggle("show");
+  first.classList.toggle("hidden2");
+  first.classList.toggle("show2");
+  second.classList.toggle("hidden2");
+  second.classList.toggle("show2");
+});
+
+var list = document.querySelectorAll('.circle');
+
+list.forEach((item) => {
+  new CircleType(item).radius(280);
 });
