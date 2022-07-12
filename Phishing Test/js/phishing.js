74b5de8a9be74286853e1aa02e8a8cb56c763b9c
nickhughes34 <101068746+nickhughes34@users.noreply.github.com>
2022-07-12 11:22:08 -0400

    Phishing test

---
+++ Phishing Test/js/phishing.js
@@ -0,0 +1,72 @@
+var content = document.getElementById("displayEmailDiv");
+var outline = document.getElementById("outline");
+var next = document.getElementById("next");
+var results = document.getElementById('results');
+
+var filename = 0
+var randomNumber = Math.floor(Math.random() * 2) + 1 // gets either 1 or 2.
+var phishing = []
+var regular = []
+
+for (i=1; i<101;i++){ //makes 2 lists fulled with numbers to 100.
+ phishing.push(i);
+ regular.push(i);
+}
+
+if (randomNumber == 1){
+  filename = phishing.pop(-1)
+  content.innerHTML='<object id = "email" type="text/html" data="../Phishing_HTML/data (' + filename + ').html" style="min-height: 690px; width: 1500px;"></object>';
+} else {
+  filename = regular.pop(-1)
+  content.innerHTML='<object id = "email" type="text/html" data="../Regular_HTML/data (' + filename + ').html" style="min-height: 690px; width: 1500px;"></object>';
+}
+
+next.addEventListener("click", function(){
+if (randomNumber == 1){
+  outline.classList.remove("green")
+  outline.classList.add("red")
+
+  results.classList.remove("green")
+  results.classList.add("red")
+  results.innerHTML = "Last email was phishing"
+} else if (randomNumber == 2){
+  outline.classList.remove("red")
+  outline.classList.add("green")
+
+  results.classList.remove("red")
+  results.classList.add("green")
+  results.innerHTML = "Last email was normal"
+} else {
+  outline.classList.remove("red")
+  outline.classList.remove("green")
+  outline.classList.add("black")
+
+  results.classList.remove("red")
+  results.classList.remove("green")
+  results.classList.add("black")
+  results.innerHTML = "Done"
+}
+
+  randomNumber = Math.floor(Math.random() * 2) + 1 // gets either 1 or 2.
+
+  if (phishing.length == 0){
+    randomNumber = 2
+  }
+
+  if (regular.length == 0){
+    randomNumber = 1
+  }
+
+   if (phishing.length == 0 && regular.length == 0){
+      content.innerHTML= "Done";
+  } else {
+    if (randomNumber == 1){
+      filename = phishing.pop(-1)
+      content.innerHTML='<object id = "email" type="text/html" data="../Phishing_HTML/data (' + filename + ').html" style="min-height: 690px; width: 1500px;"></object>';
+    } else{
+      filename = regular.pop(-1)
+      content.innerHTML='<object id = "email" type="text/html" data="../Regular_HTML/data (' + filename + ').html" style="min-height: 690px; width: 1500px;"></object>';
+    }
+  }
+
+});
---
+++ Phishing Test/js/phishing_angular.js
@@ -0,0 +1,6 @@
+var app = angular.module('MainPage', []);
+
+app.controller("MainController", function($scope){
+  $scope.example2 = ["Reading", "Coding", "Walking","Moblie Game Design","Building Computers","Solving Puzzels","Network Design","Website Design"];
+  $scope.example = "example.";
+});
