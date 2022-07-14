61de587829c0d253aa50452242d876bfdd96efbe
nickhughes34 <101068746+nickhughes34@users.noreply.github.com>
2022-07-14 15:40:06 -0400

    Removed Project

--- Phishing Test/js/phishing_angular.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('MainPage', []);
-
-app.controller("MainController", function($scope){
-  $scope.example2 = ["Reading", "Coding", "Walking","Moblie Game Design","Building Computers","Solving Puzzels","Network Design","Website Design"];
-  $scope.example = "example.";
-});
