2f0067b1a15488756e43012f87dcb6da2bf14163
nickhughes34 <101068746+nickhughes34@users.noreply.github.com>
2022-08-16 12:30:14 -0400

    Update index-angular.js

--- Src/index-angular.js
+++ Src/index-angular.js
@@ -19,11 +19,11 @@ app.controller("MainController", function($scope){
    {id:"pro3",status:"",title:"Coffee Menu", name:"Coffee Menu Webpage",image:"Src/Coffee-screenshot.png",alt:"coffee menu screenshot",description:"This project lets the user view a menu for Coffee Cafe Today. Used jQuery to setup a delayed animation for when user scrolls into view. Used Bootstrap and Angular for easy placement of elements.",code:"https://github.com/nickhughes34/nickhughes34.github.io/tree/main/Coffee",link:"https://nickhughes34.github.io/Coffee/coffee.html",tech:{html, css, javascript, angular, bootstrap, mdb}},
    {id:"pro4",status:"",title:"Weather App", name:"Weather Checker Webpage",image:"Src/Weather-screenshot.png",alt:"weather app screenshot",description:"This project lets the user input a city name to get the weather results. This project used plain Javascript to control the DOM to add all the weather elements.",code:"https://github.com/nickhughes34/nickhughes34.github.io/tree/main/Weather",link:"https://nickhughes34.github.io/Weather/weather.html",tech:{html, css, javascript}}];
 
-  $scope.description1 = "I am currently attending Ontario Tech University studying Networking and IT Security. Over the years of attending this school I have gained various skills. Some of them would include network design, python programming, front end and back end web design. I have developed some test webpages such as a Weather Checker, Product Webpage, and more listed below. I have also used various back end technologies such as API's, Amazon's RDS and MongoDB.";
+  $scope.description1 = "I am currently attending Ontario Tech University studying Networking and IT Security. Over the years of attending this school I have gained various skills. Some of them would include network design, python programming, front end and back end web design. I have developed some test webpages such as a Weather Checker, Product Webpage, and more listed below. I have also used various back end technologies such as API's, Amazon's RDS and MySQL.";
 
-$scope.proficient = ["HTML", "Javascript", "Python","CSS","jQuery","Node.js","Angular","C"];
+$scope.proficient = ["Python", "C", "Linux", "React", "HTML", "CSS", "Javascript", "jQuery", "Node.js", "Angular"];
 
-$scope.hobbies = ["Reading", "Coding", "Walking","Moblie Game Design","Building Computers","Solving Puzzels","Network Design","Website Design"];
+$scope.hobbies = ["Reading", "Coding", "Walking", "Moblie Game Design", "Building Computers", "Solving Puzzels", "Network Design", "Website Design", "Machine Learning", "Sports"];
 
   $scope.description2 = "I have developed a mobile game call Pixel Block Defense in Android Studio. I make abundant long-term and short-term goals to stay organized. I have plenty of experience with coding and network systems.";
 
